837a47aa66a003fd135b408e78ac606c0fc6402d
Irene Mateo Herrero <irenemherrero@gmail.com>
2021-11-26 15:49:46 +0100

    feat: initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from "react";
+import photoMe from "../images/foto_perfil_prop.jpg";
+
+class Hero extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="heroTextContainer">
+          <h1 className="fontPreTitle preTitleEffect">
+            Junior Front-End Developer
+          </h1>
+          <h2 className="fontTitle typewriter">Soy Irene Mateo.</h2>
+          <img
+            className="photoMe"
+            src={photoMe}
+            alt="Irene Mateo, Junior FrontEnd Developer"
+          />
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Hero;
---
+++ src/components/Presentation.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+
+class Presentation extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <div className='anchor' id="presentation" />
+                <div className="presentationText">
+                    <i className="far fa-smile"></i>
+                    <p className='paragraphTitle'>Encantada de conocerte.</p>
+                    <p className="fontText">Soy Irene Mateo Herrero, programadora Front-End Junior formada en Adalab (Madrid).
+                    </p>
+                    <p className="fontText">
+                        Tras trabajar como profesora de español en Bélgica y Portugal decidí volver a España y entonces, consciente de la importancia de dominar la tecnología en el mundo actual, me he formado en programación Front-End. Ahora busco mi primera experiencia laboral en el sector.
+                    </p>
+                    <p className="fontText">Soy una persona vital y positiva. Adaptarme a nuevos entornos y equipos es mi mayor cualidad. </p>
+                    <p className='paragraphTitle'>¿Qué puedo aportar a tu proyecto?</p>
+                </div>
+            </Fragment>
+        );
+    };
+};
+
+export default Presentation;
+
